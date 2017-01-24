08738950c2fd4385a0cc66688f0f33b9efcdf9aa
Hermes Machine <hermesbuildmachine@pcp067685pcs.wireless.calpoly.edu>
2017-01-24 10:29:51 -0800

    Fixed BunnySpawner to spawn bunnies in random spots on the plane.

--- src/ECS2/BunnySpawnSystem.cpp
+++ src/ECS2/BunnySpawnSystem.cpp
@@ -22,6 +22,52 @@ void BunnySpawnSystem::Update(long frameTime, World *world) {
         float velY = sin(randomAngle);
         glm::vec3 vel = normalize(glm::vec3(velX, 0, velY)) / glm::vec3(20, 20, 20);
         RigidBody *rigidBody = (RigidBody*)bunny->GetComponent("RigidBody");
+        
+        float floatX[maxEntities];
+        float floatZ[maxEntities];
+        vec3 randPosition;
+        GameObject obj;
+        bool positionClear = false;
+        int randX;
+        int randZ;
+        
+        
+        //collect positions
+        int i = 0;
+        for(auto &obj : world->GetGameObjects()){
+            randPosition = obj->transform->position;
+            floatX[i] = randPosition.x;
+            floatZ[i] = randPosition.z;
+            i++;
+        }
+        
+        
+        //check if positions found
+        while (!positionClear)
+        {
+            randX = rand() % 100 - 50;
+            randZ = rand() % 100 - 50;
+            
+            bool xFound = false;
+            bool zFound = false;
+            
+            //check through arrays
+            for (int j = 0; j < maxEntities; j++) {
+                if (floatX[j] == randX) {
+                    xFound = true;
+                }
+                if (floatZ[j] == randZ) {
+                    zFound = true;
+                }
+            }
+            
+            if (!(xFound && zFound)) {
+                positionClear = true;
+                randPosition = vec3(randX, 0, randZ);
+            }
+        }
+
+        bunny->transform->position = randPosition;
         rigidBody->velocity -= vel;
         rigidBody->useGravity = true;
         bunny->transform->rotation += vec3(0, (float)-(randomAngle), 0);
